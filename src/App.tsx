541a37db72999d2d2f24db487842a052d0efad02
João Pedro Alves <joaopealves.dev@gmail.com>
2025-04-13 17:41:49 -0300

    feat: add splash screen (#5)
    * feat: add splash screen component
    
    * style: update background color in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import { Kurisu } from './components/kurisu'
 import MessageBox from './components/message-box'
 import { MessageProvider } from './contexts/MessageContext'
+import { SplashScreenManager } from './components/splash-screen/manager'
 
 function App() {
   return (
     <MessageProvider>
+      <SplashScreenManager />
       <div className="flex h-screen w-screen items-center justify-center">
         {/* <MatrixBackground /> */}
 
