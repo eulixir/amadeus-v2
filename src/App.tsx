ed2a27fa23a83ce260a8842c5f1c3b52005a16ad
João Pedro Alves <joaopealves.dev@gmail.com>
2025-04-13 01:02:36 -0300

    feat: add background image and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import MatrixBackground from './components/background'
+// import MatrixBackground from './components/background'
 import { Kurisu } from './components/kurisu'
 import MessageBox from './components/message-box'
 import { MessageProvider } from './contexts/MessageContext'
@@ -7,11 +7,10 @@ function App() {
   return (
     <MessageProvider>
       <div className="flex h-screen w-screen items-center justify-center">
-        <MatrixBackground />
-        <div className="w-full h-full flex items-center justify-center">
-          <Kurisu />
-          <MessageBox />
-        </div>
+        {/* <MatrixBackground /> */}
+
+        <Kurisu />
+        <MessageBox />
       </div>
     </MessageProvider>
   )
